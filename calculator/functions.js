98309cddd9b43f3cd3788298e3ffa2660c535e6d
Talaa Zina <talazina@gmail.com>
2019-09-05 15:03:37 +0100

    rewriting add and sub functions -add nesting test in describe block

--- calculator/functions.js
+++ calculator/functions.js
@@ -1,14 +1,7 @@
 const functions = {
-	add : (num1, num2) => 
-	{
-		if (num1 && num2)
-			return num1 + num2
-		else 
-			if (num1 && !(num2))
-                return num1       
-            else 
-              return 0		
-	}, 
-	sub : (num1, num2) => num1 - num2
+	add : (num1, num2) => (num1 && num2) ? num1 + num2 : (num1 && !(num2)) ? num1 : 0  ,
+	
+	sub : (num1, num2) => (num1 && num2) ? num1 + num2 : (num1 && !(num2)) ? num1 : 0  
+
 };
 module.exports = functions;
--- calculator/functions.test.js
+++ calculator/functions.test.js
@@ -1,12 +1,23 @@
 const functions = require('./functions');
 
-test('Adds 1 + 2 to equal 3', () => {
-  expect(functions.add(1, 2)).toBe(3);
+describe('Calculator functions', () => {
+	describe('When calling add function', () => {
+		test('it should return 3 when adding 1 and 2', () => {
+            expect(functions.add(1, 2)).toBe(3);
+		});
+
+		test ('it should not return 4 when adding 1 and 2 ', () => {
+			expect(functions.add(1, 2)).not.toBe(4);
+		});
+		test ('it should return the value of the argument when thre is one argument', () => {
+			expect(functions.add(1)).toBe(1)
+		});
+	});
+
+	describe('When calling sub function', () => {});
+
+	describe('When calling multiply function', () => {});
+
+	describe('When calling divide function', () => {});
 });
 
-test ('Adds 1 + 2 to not equal 4', () => {
-	expect(functions.add(1, 2)).not.toBe(4);
-})
-test ('return the value of the argument when thre is one argument', () => {
-	expect(functions.add(1)).toBe(1)
-})
