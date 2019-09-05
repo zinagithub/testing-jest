f025dc20a643ee930438220c9ae66b2e6fb6d1d3
Talaa Zina <talazina@gmail.com>
2019-09-05 16:13:16 +0100

    add sub and multiply tests

--- calculator/functions.js
+++ calculator/functions.js
@@ -1,7 +1,11 @@
 const functions = {
 	add : (num1, num2) => (num1 && num2) ? num1 + num2 : (num1 && !(num2)) ? num1 : 0  ,
 	
-	sub : (num1, num2) => (num1 && num2) ? num1 + num2 : (num1 && !(num2)) ? num1 : 0  
+	sub : (num1, num2) => (num1 && num2) ? num1 - num2 : (num1 && !(num2)) ? num1 : 0  ,
+
+	multiply : (num1, num2) => (num1 && num2) ? num1 * num2 : (num1 && !(num2)) ? num1 : 0  ,
+
+	devide   : (num1, num2) => (num1 && num2 && num2 != 0) ? num1 / num2 : (num1 && !(num2)) ? num1 : 0
 
 };
 module.exports = functions;
--- calculator/functions.test.js
+++ calculator/functions.test.js
@@ -1,4 +1,5 @@
 const functions = require('./functions');
+//test('', () => {});
 
 describe('Calculator functions', () => {
 	describe('When calling add function', () => {
@@ -9,14 +10,57 @@ describe('Calculator functions', () => {
 		test ('it should not return 4 when adding 1 and 2 ', () => {
 			expect(functions.add(1, 2)).not.toBe(4);
 		});
-		test ('it should return the value of the argument when thre is one argument', () => {
+		test ('it should return the value of the argument when there is one argument', () => {
 			expect(functions.add(1)).toBe(1)
+		});
+		test('adding floating point numbers', () => {
+			expect(functions.add(0.1, 0.2)).toBeCloseTo(0.3); 
+		});
+		test('it should return 0 if no argument is specified', () => {
+			expect(functions.add()).toBeFalsy();
 		});
 	});
 
-	describe('When calling sub function', () => {});
+	describe('When calling sub function', () => {
+		test('it should return 1 when subtract 1 from 2', () => {
+			expect(functions.sub(2, 1)).toBe(1);
+		});
+		test('it should return negative result  when subtract 2 from 1', () => {
+			expect(functions.sub(1, 2)).toBeLessThan(1);
+		});
+		test ('it should return the value of the argument when there is one argument', () => {
+			expect(functions.sub(1)).toBe(1)
+		});
+		test('adding floating point numbers', () => {
+  			expect(functions.sub(0.2, 0.1)).toBeCloseTo(0.1); 
+		});
+		test('it should return 0 if no argument is specified', () => {
+			expect(functions.sub()).toBeFalsy();
+		});
 
-	describe('When calling multiply function', () => {});
+	});
+
+	describe('When calling multiply function', () => {
+		test('it should return 16 when multiply 4 by 4', () => {
+			expect(functions.multiply(4, 4)).toBe(16);
+		});
+		test('it should return positive value when multiply -4 by -4 or 4 by 4', () => {
+			expect(functions.multiply(-4, -4)).toBeGreaterThan(0);
+			expect(functions.multiply(4, 4)).toBeGreaterThan(0);
+		});
+		test('it should return negative value when multiply -4 by -4', () => {
+			expect(functions.multiply(4, -4)).toBeLessThan(0);
+		});
+		test('it should return 0 if one argument is equal to zero specified', () => {
+			expect(functions.multiply(0, 999)).toBeFalsy();
+		});
+		test('it should return 0 if no argument is specified', () => {
+			expect(functions.multiply()).toBeFalsy();
+		});
+		test ('it should return the value of the argument when there is one argument', () => {
+			expect(functions.multiply(1)).toBe(1)
+		});
+	});
 
 	describe('When calling divide function', () => {});
 });
