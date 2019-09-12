091affc8e8f9b04db2a5aaaf941cbfe39516d8cf
Talaa Zina <talazina@gmail.com>
2019-09-12 11:00:27 +0100

    add fixes to correct stickler errors

--- calculator/functions.js
+++ calculator/functions.js
@@ -2,46 +2,38 @@ const functions = {
   add: (num1, num2) => {
     if (num1 && num2) {
       return num1 + num2;
-    } else {
-      if (num1 && !num2) {
-        return num1;
-      } else {
-        return 0;
-      }
     }
+    if (num1 && !num2) {
+        return num1;
+    }
+    return 0;
   },
   sub: (num1, num2) => {
     if (num1 && num2) {
       return num1 - num2;
-    } else {
-      if (num1 && !num2){
-        return num1;
-      } else {
-        return 0;
-      }
     }
+    if (num1 && !num2){
+      return num1;
+    }
+    return 0;
   },
   multiply: (num1, num2) => {
     if (num1 && num2){
       return num1 * num2;
-    } else {
-      if (num1 && !num2) {
-        return num1;
-        } else {
-            return 0;
-        }
     }
+    if (num1 && !num2) {
+      return num1;
+    }  
+    return 0;
   },
   devide: (num1, num2) => {
     if (num1 && num2 && num2 !== 0) {
       return num1 / num2;
-    } else {
-        if (num1 && !num2) {
-          return 'infinity';
-        } else {
-        return 0;
-        }
     }
+    if (num1 && !num2) {
+      return 'infinity';
+    }
+    return 0;
   }
 };
 module.exports = functions;
